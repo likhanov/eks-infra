262153062ed691cf4de6f739d6524161fa683b55
Vladimir Likhanov <vladimir.likhanov@gmail.com>
2024-11-05 20:50:45 +0300

    fix providers for karpenter

---
+++ modules/karpenter/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.1.2"
+    }
+  }
+}
