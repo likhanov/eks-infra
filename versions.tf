02a4a536c4316eb9765633f3db1e0cda319dff77
Vladimir Likhanov <vladimir.likhanov@gmail.com>
2024-11-05 21:05:04 +0300

    fix providers for karpenter

--- versions.tf
+++ versions.tf
@@ -6,5 +6,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.1.2"
+    }
   }
 }
